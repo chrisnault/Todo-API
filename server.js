570dd364942a28cb056ebf3797821ca72ebdafe8
Chris Nault <chrisnault@gmail.com>
2016-03-25 09:47:32 -0500

    Refactor todos with sequelize

--- server.js
+++ server.js
@@ -17,44 +17,27 @@ app.get('/', function(req, res) {
 // GET /todos?completed=true&q=house
 app.get('/todos', function(req, res) {
 
-	var queryParams = req.query;
-	var filteredTodos = [];
- 	// db.todo.create(body).then(function (todo) {
-		// res.json(todo.toJSON());
- 	// }, function(e) {
- 	// 	res.status(400).json(e);
- 	// })
-	db.todo.findAll({
-		attributes: ['description', 'completed']
-	}).then(function (todo) {
-		filteredTodos = todo.toJSON();
+	var query = req.query;
+	var where = {};
+
+	if (query.hasOwnProperty('completed') && query.completed === 'true') {
+		where.completed = true;
+	} else if (query.hasOwnProperty('completed') && query.completed === 'false') {
+		where.completed = false;
+	}
+
+	if (query.hasOwnProperty('q') && query.q.length > 0) {
+		where.description = {
+			$like: '%' + query.q + '%'
+		};
+	}
+
+	db.todo.findAll({where: where}).then(function (todos) {
+		res.json(todos)
 	}, function (e) {
-		res.status(400).json(e);
+		res.status(500).send();
 	})
 
-	if (queryParams.hasOwnProperty('completed') && queryParams.completed.length > 0) {
-
-		if (queryParams.completed === 'true') {
-			queryParams.completed = true;
-		} else {
-			queryParams.completed = false;
-		}
-
-		filteredTodos = _.where(filteredTodos, {
-			completed: queryParams.completed
-		});
-	}
-
-	if (queryParams.hasOwnProperty('q') && queryParams.q.length > 0) {
-		filteredTodos = _.filter(filteredTodos, function(todo) {
-			return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1;
-			// if (todo.description.indexOf(queryParams.q) > 0) {
-			// 	return todo;
-			// }
-		});
-	}
-
-	res.json(filteredTodos);
 });
 
 // GET /todos/:id
