e1d66fd4201c903ce71c083deb6379ceb9e61eb5
Chris Nault <chrisnault@gmail.com>
2016-04-03 21:14:59 -0500

    add generateToken to user model

--- server.js
+++ server.js
@@ -211,7 +211,12 @@ app.post('/users/login', function (req, res) {
 	var body = _.pick(req.body, 'email', 'password');
 	
 	db.user.authenticate(body).then(function (user) {
-		res.status(200).json(user.toPublicJSON());
+		var token = user.generateToken('authentication');
+		if (token) {
+			res.header('Auth', token).status(200).json(user.toPublicJSON());	
+		} else {
+			res.status(401).send();
+		}
 	}, function () {
 		res.status(401).send();
 	});
